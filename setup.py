c26580c213eaa97a8412fcb94ef0d60d3f11d84b
= <geekychav@gmail.com>
2025-06-19 21:55:35 +0100

    feature-setup - add a setup.yaml for installing into bop codebases

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="bop_common",
+    version="0.1.0",
+    packages=find_packages(),
+    description="Holds shared modules for the bop_sense, bop_brain and bop_body codebases",
+    author="Shane Chris Barker",
+    author_email="geekychav@gmail.com",
+    python_requires=">=3.12"
+)
