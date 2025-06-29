6ccfcb5027cb4542f2b17d4f90bd0e781b6990f4
= <geekychav@gmail.com>
2025-06-29 20:42:10 +0100

    remove commar

--- bop_common/enums/hardware_type.py
+++ bop_common/enums/hardware_type.py
@@ -3,7 +3,6 @@ from enum import Enum
 class HardwareType(Enum):
     MIC = 'mic'
     CAMERA = 'camera'
-    TEST_HARDWARE = 'test',
-    SCREEN = 'screen',
-    BODY_SPEAKER = 'body_speaker',
-
+    TEST_HARDWARE = 'test'
+    SCREEN = 'screen'
+    BODY_SPEAKER = 'body_speaker'
