fa6aed24f4528c715a22308e8f3e9c4c52922841
= <geekychav@gmail.com>
2025-06-29 20:41:10 +0100

    add more hardware enums

--- bop_common/enums/hardware_type.py
+++ bop_common/enums/hardware_type.py
@@ -3,5 +3,7 @@ from enum import Enum
 class HardwareType(Enum):
     MIC = 'mic'
     CAMERA = 'camera'
-    TEST_HARDWARE = 'test'
+    TEST_HARDWARE = 'test',
+    SCREEN = 'screen',
+    BODY_SPEAKER = 'body_speaker',
 
